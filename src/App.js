d02f22cdf88ffa3171ff8e3cf22388c725153280
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-02-28 23:38:53 +0500

    add first

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [arrayNew, setnewState] = useState(["items 1", "items 2"]);
+
+  function addItems() {
+    const newarray = `items ${arrayNew.length + 1}`;
+    setnewState((pre) => {
+      return [...pre, newarray];
+    });
+  }
+  const newArray = arrayNew.map((item, index) => <div key={index}>{item}</div>);
+  return (
+    <div className="App">
+      {newArray}
+      <button onClick={addItems}>add items</button>
+    </div>
+  );
+}
+
+export default App;
